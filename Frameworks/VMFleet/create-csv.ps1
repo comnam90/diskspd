00fdb9b3651636b13550b86fd58d20f39d3b2cc6
Ben Thomas <ben.charles@gmail.com>
2016-05-14 18:05:34 +1200

    Fixed create-csv.ps1

--- Frameworks/VMFleet/create-csv.ps1
+++ Frameworks/VMFleet/create-csv.ps1
@@ -25,7 +25,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 #>
 param(
-    [string[]]$nodes = @()
+    [string[]]$nodes = @(),
     [string]$storagepool = $null
     )
 
